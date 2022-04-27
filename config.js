9e06b1e09c41eedf9bdad8e602adc036f410702e
Jenky1031 <51738115+Jenky1031@users.noreply.github.com>
2022-04-27 17:47:01 +0800

    .

---
+++ config.js
@@ -0,0 +1,5 @@
+window.config = {
+	POST_BASE_URL: "https://app.mokahr.com/campus_apply/wenext/45156#/job", // 投递职位
+	API_BASE_URl: "https://api.mokahr.com/api-platform/v1", // API
+	IMG_BASE_URL: "https://wenext-recruitment.oss-cn-beijing.aliyuncs.com/images", // 图片
+}
